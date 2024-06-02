72e4decebf057f9720e677713b0c8e0dcb2b0c5c
Ignacio Lutteri <ignaciolutteri@gmail.com>
2024-06-02 20:11:36 -0300

    router conf

--- src/router.js
+++ src/router.js
@@ -3,10 +3,10 @@ import Formulario from './components/Form.vue';
 import Usuarios from './components/Usuarios.vue';
 
 const routes = [
-  { path: '/', component: Usuarios },
+  { path: '/', component: Formulario },
   { path: '/form', component: Formulario },
   { path: '/usuarios', component: Usuarios },
-  { path: '/:pathMatch(.*)*', component: Usuarios }
+  { path: '/:pathMatch(.*)*', component: Formulario }
 ];
 
 const router = createRouter({
